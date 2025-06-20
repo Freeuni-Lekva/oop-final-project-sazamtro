9cf2b47ac4cee3e064e2f1f3bd86d4240326011e
tshilakadze <tshil23@freeuni.edu.ge>
2025-06-20 21:07:09 +0400

    User class and pom

--- Quizzes/src/main/java/bean/User.java
+++ Quizzes/src/main/java/bean/User.java
@@ -1,4 +1,32 @@
 package bean;
 
 public class User {
+    private int user_id;
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    public User(int id, String name, String password, boolean b){
+        this.user_id = id;
+        this.username = name;
+        this.password = password;
+        this.isAdmin = b;
+    }
+
+    public int getUserId(){
+        return user_id;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public boolean checkIfAdmin(){
+        return isAdmin;
+    }
+
 }
