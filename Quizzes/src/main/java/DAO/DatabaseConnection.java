0ac2487a62ea0d45324698b59481e90c8041c0d5
Irakli Zerekidze <izere23@freeuni.edu.ge>
2025-06-20 20:59:09 +0400

    Added DatabaseConnection class modified pom.xml

---
+++ Quizzes/src/main/java/DAO/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package DAO;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/sazamtro";
+    private static final String ACCOUNT = "root";
+    private static final String PASSWORD = "AlfredTheButtler2006";
+
+    public static Connection getConnection(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, ACCOUNT, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
