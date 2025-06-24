04ff4e6c94395336260342605740b78e788c6edb
Gvantsa_vakh <gvanca05.vax@gmail.com>
2025-06-24 14:41:58 +0400

    added some simple methods and bean classes

---
+++ Quizzes/src/main/java/bean/AnswerOptions.java
@@ -0,0 +1,13 @@
+package bean;
+
+public class AnswerOptions {
+    private int id;
+    private int questionId;
+    private String answerText; //shemosaxazis teqsti
+    private boolean correctAnswer;
+    //option position sheidzleba
+
+
+
+
+}
