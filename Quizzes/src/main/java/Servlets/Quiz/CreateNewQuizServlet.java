4a6e931d5265bb13f8e4d86d1f78324dcc40d90a
tshilakadze <tshil23@freeuni.edu.ge>
2025-07-09 17:30:30 +0400

    jsp for creating quiz

--- Quizzes/src/main/java/Servlets/Quiz/CreateNewQuizServlet.java
+++ Quizzes/src/main/java/Servlets/Quiz/CreateNewQuizServlet.java
@@ -5,6 +5,7 @@ import DAO.QuizDAO;
 import bean.User;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@WebServlet("/quizzes/new")
 public class CreateNewQuizServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
